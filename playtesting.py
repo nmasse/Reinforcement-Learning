cf267773b9ccd86429229f3b0a1ad64924dab13b
gdgrant <gdgrant@uchicago.edu>
2018-04-24 22:36:37 -0500

    Added an interface to playtest the stimulus

---
+++ playtesting.py
@@ -0,0 +1,22 @@
+import stimulus
+import numpy as np
+
+env = stimulus.RoomStim()
+
+# self.action_dict = {0:'up', 1:'down', 2:'left', 3:'right', 4:'door'}
+
+while True:
+    act = input('--> ')
+    if act == 'w':
+        action = [1,0,0,0,0]
+    elif act == 's':
+        action = [0,1,0,0,0]
+    elif act == 'a':
+        action = [0,0,1,0,0]
+    elif act == 'd':
+        action = [0,0,0,1,0]
+    elif act == 'e':
+        action = [0,0,0,0,1]
+    elif act == 'q':
+        quit()
+    env.action(np.array(action))
